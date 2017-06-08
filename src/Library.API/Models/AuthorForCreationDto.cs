9d9c15c15320e6572f9181df2b894232effb4e48
Jim Shaw <jimxshaw@gmail.com>
2017-06-08 14:41:53 -0500

    Added property to AuthorForCreationDto.

--- src/Library.API/Models/AuthorForCreationDto.cs
+++ src/Library.API/Models/AuthorForCreationDto.cs
@@ -16,5 +16,8 @@ namespace Library.API.Models
 
         public string Genre { get; set; }
 
+        public ICollection<BookForCreationDto> Books { get; set; } = new List<BookForCreationDto>();
+
+
     }
 }
