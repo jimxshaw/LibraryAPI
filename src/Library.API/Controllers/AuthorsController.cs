724c6e31449c3de7e8be9b2a81b634a0d21bf603
Jim Shaw <jimxshaw@gmail.com>
2017-06-08 15:56:08 -0500

    Added action to prevent creating an Author on an existing guid.

--- src/Library.API/Controllers/AuthorsController.cs
+++ src/Library.API/Controllers/AuthorsController.cs
@@ -3,6 +3,7 @@ using Library.API.Entities;
 using Library.API.Helpers;
 using Library.API.Models;
 using Library.API.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -84,6 +85,17 @@ namespace Library.API.Controllers
             // The action called is GetAuthor, which needs an id for its route. 
             // The actual author that'll be serialized to the response body is the returned author dto.
             return CreatedAtRoute("GetAuthor", new { id = authorToReturn.Id }, authorToReturn);
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult BlockAuthorCreation(Guid id)
+        {
+            if (_libraryRepository.AuthorExists(id))
+            {
+                return new StatusCodeResult(StatusCodes.Status409Conflict);
+            }
+
+            return NotFound();
         }
     }
 }
