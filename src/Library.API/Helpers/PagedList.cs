a7ec7f0ccffec6589240b268613e638a2021563c
Jim Shaw <jimxshaw@gmail.com>
2017-06-29 16:26:52 -0500

    Created generic PagedList class to be used with pagination metadata.

---
+++ src/Library.API/Helpers/PagedList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.API.Helpers
+{
+    public class PagedList<T> : List<T>
+    {
+        public int CurrentPage { get; private set; }
+
+        public int TotalPages { get; private set; }
+
+        public int PageSize { get; private set; }
+
+        public int TotalCount { get; private set; }
+
+        public bool HasPrevious
+        {
+            get
+            {
+                return (CurrentPage > 1);
+            }
+        }
+
+        public bool HasNext
+        {
+            get
+            {
+                return (CurrentPage < TotalPages);
+            }
+        }
+
+        public PagedList(List<T> items, int count, int pageNumber, int pageSize)
+        {
+            TotalCount = count;
+            PageSize = pageSize;
+            CurrentPage = pageNumber;
+            // If we have 101 items then total pages will be 11.
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            // This add all the items to the underlying list.
+            AddRange(items);
+        }
+
+        public static PagedList<T> Create(IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            var count = source.Count();
+            var items = source.Skip((pageNumber - 1) * pageSize)
+                              .Take(pageSize)
+                              .ToList();
+
+            return new PagedList<T>(items, count, pageSize, pageSize);
+        }
+    }
+}
